6ea164b591854516b8aa8617fb9672e63490b91d
joe <okoshi811@gmail.com>
2023-10-21 15:10:16 +0900

    1_7_1 complete

--- sources/ALDS_1_7_A.rb
+++ sources/ALDS_1_7_A.rb
@@ -7,23 +7,51 @@ class Node
     @left = left
     @right = right
   end
+
+  def childIds
+    childIds = []
+    ctr = self.left
+    while ctr != nil do
+      childIds.push(ctr.id)
+      ctr = ctr.right
+    end
+    return childIds.to_s
+  end
+
+  def depth
+    num = 0
+    ctr = self
+    while ctr.parent != nil && ctr.parent != -1
+      num += 1
+      ctr = ctr.parent
+    end
+    return num
+  end
+
+  def kind
+    if self.parent == -1
+      "root"
+    elsif self.left == nil
+      "leaf"
+    else
+      "internal node"
+    end
+  end
+
 end
 
 n = gets().to_i
 tree = []
 (0...n).each do |i|
-  tree.append(Node.new)
+  tree.push(Node.new(id: i))
 end
 
 (0...n).each do
   line = gets.split(' ')
   id = line[0].to_i
   k = line[1].to_i
-  tree[id].id = id
   
-  if id == 0
-    tree[id].parent = -1
-  end
+  tree[id].parent = -1 if tree[id].parent.nil? 
 
   l = nil
   (0...k).each do |j|
@@ -40,9 +68,9 @@ end
 end
 
 (0...n).each do |i|
-  if tree[i].parent != -1
-    puts "#{tree[i].id}, #{tree[i].parent.id}"
+  if tree[i].parent == -1
+    puts "node #{i}: parent = #{tree[i].parent}, depth = #{tree[i].depth}, #{tree[i].kind}, #{tree[i].childIds}"
   else
-    puts "#{tree[i].id}, #{tree[i].parent}"
+    puts "node #{i}: parent = #{tree[i].parent.id}, depth = #{tree[i].depth}, #{tree[i].kind}, #{tree[i].childIds}"
   end
 end
